5027e214ccb947deb5d74c4788456479879b77b9
Levi <Daz>
2025-01-17 17:53:00 -0600

    designed home page

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
 
-const session = require('express-session');
-const csrf = require('csurf');
-const crypto = require('crypto');
-const bodyParser = require('body-parser');
-const path = require('path');
+app.use("", require("./routes/home.routes"))
 
+const port = 3000;
 
-app.use(session({
-    secret: 'aF*AWE*$T78"rfsfgawe%*',
-    resave: false,
-    saveUninitialized: false,
-  }));
-
-// Middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-const csrfProtection = csrf();
-app.use(csrfProtection);
-
-app.use((request, response, next) => {
-    response.locals.csrfToken = request.csrfToken(); 
-    next();
-});
-
-app.listen(4000, () => {
-    console.log("Example app listening on port 4000")
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
 })
