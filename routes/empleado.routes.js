5261a69163d62508d9f9a23692ed619bbdad1746
Levi <Daz>
2025-02-21 21:23:57 -0600

    added authentication requirement to routes

--- routes/empleado.routes.js
+++ routes/empleado.routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const contratoController = require('../controllers/contrato.controller');
+const isAuth = require('../util/is-auth');
 const router = express.Router();
 
-router.get('/cartera', contratoController.get_contratos);
+router.get('/cartera', isAuth, contratoController.get_contratos);
 // router.post('/crear-contrato', contratoController.post_crear_contrato);
-router.get('/cartera/buscar/', contratoController.get_buscar_contrato);
-router.get('/cartera/buscar/:valor_busqueda', contratoController.get_buscar_contrato);
-router.get('/crear-contrato', contratoController.get_tratamientos, contratoController.get_armazones);
-router.get('/crear-contrato/get-json', contratoController.get_json);
-router.post('/crear-contrato', contratoController.post_contrato);
+router.get('/cartera/buscar/', isAuth, contratoController.get_buscar_contrato);
+router.get('/cartera/buscar/:valor_busqueda', isAuth, contratoController.get_buscar_contrato);
+router.get('/crear-contrato', isAuth, contratoController.get_tratamientos, contratoController.get_armazones);
+router.get('/crear-contrato/get-json', isAuth, contratoController.get_json);
+router.post('/crear-contrato', isAuth, contratoController.post_contrato);
 
 router.get('/test', (req, res) => {
     console.log('Test route hit!');
--- util/is-auth.js
+++ util/is-auth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if(!req.session.isLoggedIn){
+    if(!req.session.sesionIniciada){
         return res.redirect('/user/login');
     }
     next();
