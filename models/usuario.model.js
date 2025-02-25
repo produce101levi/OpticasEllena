ae3f918f8d85113b5684efca20cd16f537663b1c
Levi <Daz>
2025-02-25 16:37:19 -0600

    added sign up functionality

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -1,6 +1,35 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
 
 module.exports = class Usuario {
+
+    constructor(
+        mi_username, mi_nombre, mi_apellido,
+        mi_telefono, mi_correo, mi_fecha_nacimiento,
+        mi_contrasena
+    ){
+        this.username = mi_username;
+        this.nombre = mi_nombre;
+        this.apellido = mi_apellido;
+        this.telefono = mi_telefono;
+        this.correo = mi_correo;
+        this.fecha_nacimiento = mi_fecha_nacimiento;
+        this.contrasena = mi_contrasena;
+    }
+
+    registrarUsuario(){
+        return db.execute(
+            `INSERT INTO usuarios(
+                username, contrasena, nombre, apellido, telefono, 
+                correo, fecha_nacimiento
+            ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            [this.username, this.contrasena, this.nombre, this.apellido, this.telefono,
+            this.correo, this.fecha_nacimiento]
+        )
+    }
+
+
     static validarUsuario(username, contrasena){
         // console.log("MODEL Username:", username);
         return db.execute(`
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/login', userController.getLogin);
 router.post('/login', userController.postLogin);
 router.get('/registrar', userController.getRegistrar);
+router.post('/registrar', userController.postRegistrar);
 router.get('/logout', userController.getLogout);
 
 
