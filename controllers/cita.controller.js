c4ca95f6bf95a18edc99aa9a0f5d81f71c721425
Levi <Daz>
2025-02-27 09:41:19 -0600

    added MVC structure for agendar cita use case

---
+++ controllers/cita.controller.js
@@ -0,0 +1,1 @@
+const Cita = require('../models/cita.model');
---
+++ routes/cita.routes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const citaController = require('../controllers/cita.controller');
+const isAuth = require('../util/is-auth');
+const router = express.Router();
