2aa37a040683c171aae3f1398becac063927cfd3
Levi <Daz>
2025-03-04 17:53:43 -0600

    created a session variable for PROPIO

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/agendar-cita', isAuth, citaController.getAgendarCita);
 router.get('/agendar-cita/otro-usuario', isAuth, citaController.getAgendarOtro);
+router.get('/agendar-cita/propio-usuario', isAuth, citaController.getAgendarPropio);
 
 
 module.exports = router;
