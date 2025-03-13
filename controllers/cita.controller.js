309fe6094da520b3e1e22e6a66b4a22634f12bd9
Levi <Daz>
2025-03-12 19:52:57 -0600

    user can now schedule appointments in their own name

--- controllers/cita.controller.js
+++ controllers/cita.controller.js
@@ -4,6 +4,7 @@ const Usuario = require('../models/usuario.model');
 // Middleware para cargar página de "Agendar Cita"
 exports.getAgendarCita = async (req, res, next) => {
     try {
+        const edad = await Usuario.getEdadUsuario(req.session.username);
         Usuario.getInfoUsuario(req.session.username)
         .then(([users, fieldData]) => {
             // console.log(users);
@@ -16,6 +17,7 @@ exports.getAgendarCita = async (req, res, next) => {
             req.session.propio = true;
             res.render('agendar_cita', {
                 confirmed: false,
+                edad: edad,
                 propio: req.session.propio,
                 user: user,
                 name: req.session.name,  
@@ -29,6 +31,21 @@ exports.getAgendarCita = async (req, res, next) => {
 
 exports.getAgendarOtro = async (req, res, next) => {
     try {
+            req.session.propio = false;
+            return res.status(200).json({
+                confirmed: false,
+                propio: req.session.propio,
+                name: req.session.name,  
+                error: req.session.error
+            });
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+exports.getAgendarPropio = async (req, res, next) => {
+    try {
+        const edad = await Usuario.getEdadUsuario(req.session.username);
         Usuario.getInfoUsuario(req.session.username)
         .then(([users, fieldData]) => {
             // console.log(users);
@@ -36,9 +53,10 @@ exports.getAgendarOtro = async (req, res, next) => {
                 ...user,
                 fecha_nacimiento: new Intl.DateTimeFormat('es-ES', { dateStyle: 'medium' }).format(new Date(user.fecha_nacimiento)),
             }));
+            req.session.propio = true;
             const user = userFormato[0];
-            req.session.propio = false;
             return res.status(200).json({
+                edad: edad,
                 confirmed: false,
                 propio: req.session.propio,
                 user: user,
@@ -51,26 +69,29 @@ exports.getAgendarOtro = async (req, res, next) => {
     }
 }
 
-exports.getAgendarPropio = async (req, res, next) => {
+exports.postAgendarPropio = async (req, res, next) => {
     try {
+        const edad = await Usuario.getEdadUsuario(req.session.username);
         Usuario.getInfoUsuario(req.session.username)
-        .then(([users, fieldData]) => {
-            // console.log(users);
-            const userFormato = users.map(user => ({
+        .then(([usuarios, fieldData]) => {
+            const usuarioFormato = usuarios.map(user => ({
                 ...user,
                 fecha_nacimiento: new Intl.DateTimeFormat('es-ES', { dateStyle: 'medium' }).format(new Date(user.fecha_nacimiento)),
             }));
-            req.session.propio = true;
-            const user = userFormato[0];
-            return res.status(200).json({
-                confirmed: false,
-                propio: req.session.propio,
-                user: user,
-                name: req.session.name,  
-                error: req.session.error
-            });
-        })
-    } catch(error) {
-        console.log(error);
+            const usuarioActual = usuarioFormato[0];
+            if (req.body.fecha_cita === '') req.body.fecha_cita = null;
+            Cita.agendarCitaPropio(req.session.username, usuarioActual.nombre, usuarioActual.apellido, usuarioActual.telefono, edad, req.body.fecha_cita);
+
+        });
+    } catch (error){
+        console.log(error)
+    }
+}
+
+exports.postAgendarOtro = async (req, res, next) => {
+    try {
+        
+    } catch(error){
+        console.log(error)
     }
 }
