4c6c2934b2503f2eca0fe5490f840fbb285b44f3
Levi <Daz>
2025-03-25 07:48:00 -0600

    added permissions parameter

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const citaController = require('../controllers/cita.controller');
+
 const isAuth = require('../util/is-auth');
+const canAgendarCita = require('../util/can-agendar-cita');
 const router = express.Router();
 
 // Agendar Citas
-router.get('/agendar-cita', isAuth, citaController.getAgendarCita);
-router.get('/agendar-cita/otro-usuario', isAuth, citaController.getAgendarOtro);
-router.get('/agendar-cita/propio-usuario', isAuth, citaController.getAgendarPropio);
-router.post('/agendar-cita/otro-usuario', isAuth, citaController.postAgendarOtro);
-router.post('/agendar-cita/propio-usuario', isAuth, citaController.postAgendarPropio);
+router.get('/agendar-cita', isAuth, canAgendarCita, citaController.getAgendarCita);
+router.get('/agendar-cita/otro-usuario', isAuth, canAgendarCita, citaController.getAgendarOtro);
+router.get('/agendar-cita/propio-usuario', isAuth, canAgendarCita, citaController.getAgendarPropio);
+router.post('/agendar-cita/otro-usuario', isAuth, canAgendarCita, citaController.postAgendarOtro);
+router.post('/agendar-cita/propio-usuario', isAuth, canAgendarCita, citaController.postAgendarPropio);
 
 // Consultar Citas
 router.get('/consultar-cita', isAuth, citaController.getConsultarCitaPropia);
