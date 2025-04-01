7e4ac1c7e31720bac1b7c91a3c6f9de05e920a75
Levi <Daz>
2025-04-01 08:28:30 -0600

    added password safety verification

---
+++ public/scripts/registrar_cuenta.js
@@ -0,0 +1,31 @@
+// document.getElementById("password").addEventListener("input", function() {
+//     let password = this.value;
+//     let message = document.getElementById("message");
+    
+//     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+//     if (regex.test(password)) {
+//         message.style.color = "green";
+//         message.innerText = "✅ Strong password!";
+//     } else {
+//         message.style.color = "red";
+//         message.innerText = "❌ Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, a number, and a special character.";
+//     }
+// });
+
+const inputContrasena = document.getElementById("contrasena");
+
+inputContrasena.addEventListener("input", function() {
+    let contrasena = this.value;
+    let mensaje = document.getElementById("mensaje");
+
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+    if (regex.test(contrasena)) {
+        inputContrasena.classList.remove("is-danger")
+        inputContrasena.classList.add("is-success")
+    } else {
+        inputContrasena.classList.remove("is-success")
+        inputContrasena.classList.add("is-danger")
+    }
+})
