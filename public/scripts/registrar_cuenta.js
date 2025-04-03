4645272c4b77b8fc60805c3df609728307363d28
Levi <Daz>
2025-04-03 09:04:53 -0600

    fix: user clicks once to toggle & eye changes

--- public/scripts/registrar_cuenta.js
+++ public/scripts/registrar_cuenta.js
@@ -24,13 +24,16 @@ inputContrasena.addEventListener("input", function() {
 
 // Mostrar contraseña
 const mostrar = document.getElementById('mostrar_contrasena')
+const ojo = document.getElementById('ojo');
 
-mostrar.addEventListener("mousedown", () => {
-    console.log("Mouse down");
-    inputContrasena.type="text";
-})
-
-mostrar.addEventListener("mouseup", () => {
-    console.log("Mouse up");
-    inputContrasena.type="password";
+mostrar.addEventListener("click", () => {
+    if (inputContrasena.type == "text"){
+        inputContrasena.type="password";
+        ojo.classList.remove("fa-eye-slash");
+        ojo.classList.add("fa-eye");
+    } else if (inputContrasena.type="password"){
+        inputContrasena.type="text";
+        ojo.classList.remove("fa-eye");
+        ojo.classList.add("fa-eye-slash");
+    }
 })
