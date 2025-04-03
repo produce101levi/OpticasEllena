2c403588d903209423a6ce01a734217aa7cecf96
Levi <Daz>
2025-04-03 09:33:46 -0600

    fix: switched eye icon toggles

--- public/scripts/registrar_cuenta.js
+++ public/scripts/registrar_cuenta.js
@@ -41,11 +41,11 @@ const ojo = document.getElementById('ojo');
 mostrar.addEventListener("click", () => {
     if (inputContrasena.type == "text"){
         inputContrasena.type="password";
-        ojo.classList.remove("fa-eye-slash");
-        ojo.classList.add("fa-eye");
-    } else if (inputContrasena.type="password"){
-        inputContrasena.type="text";
         ojo.classList.remove("fa-eye");
         ojo.classList.add("fa-eye-slash");
+    } else if (inputContrasena.type="password"){
+        inputContrasena.type="text";
+        ojo.classList.remove("fa-eye-slash");
+        ojo.classList.add("fa-eye");
     }
 })
